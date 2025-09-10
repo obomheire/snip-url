7f5abf49c05980aecd7d0b2acf2eacc982fa0fab
Zack Bello <zackoverflow@Zacks-MacBook-Air.local>
2025-09-10 21:31:02 +0100

    Added delte converastion

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,11 +76,11 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://spurl.vercel.app/${link}`}
+            href={`https://urlp.netlify.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
-            https://spurl.vercel.app/{link}
+            https://urlp.netlify.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -97,7 +97,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://spurl.vercel.app/${link}`)
+                navigator.clipboard.writeText(`https://urlp.netlify.app/${link}`)
               }
             >
               <Copy />
